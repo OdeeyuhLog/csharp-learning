4d17c175859ec252649c6d1d38875cbf40e3ab46
Nicoco <91542808+OdeeyuhLog@users.noreply.github.com>
2024-12-06 18:20:41 +0800

    Add Number Guesser Project

---
+++ Beginner/NumberGuesser/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool numberGuessed = false;
+            int userInputNumber, randomGeneratedNumber;
+
+            Random rand = new Random();
+
+            randomGeneratedNumber = rand.Next(100);
+
+            while (!numberGuessed)
+            {
+                Console.Write("Enter a number: ");
+                userInputNumber = Int32.Parse(Console.ReadLine());
+
+                if (userInputNumber == randomGeneratedNumber)
+                {
+                    numberGuessed = true;
+                    Console.WriteLine("You guessed the random number!");
+                }
+                else
+                {
+                    if (userInputNumber < randomGeneratedNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+
+                    if (userInputNumber > randomGeneratedNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                }
+
+
+            }
+        }
+    }
+}
